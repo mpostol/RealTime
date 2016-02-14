fb7261d6c332391b710d08a4beb2c0640ae0a7f9
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2016-02-14 20:47:17 +0000

    Architecture improvements - working on the project PR36-CAS_MAIN_CORE_PCKG => CAS.RealTime Tested,
    Issue #: 4772

--- VS/trunk/CAS.RealTime/CAS.RealTimeEctension.UnitTests/SortedTreeTest.cs
+++ VS/trunk/CAS.RealTime/CAS.RealTimeEctension.UnitTests/SortedTreeTest.cs
@@ -294,7 +294,7 @@ namespace CAS.Lib.RTLibCom.Tests
       SortedTree<ClassAsTreeValue<string>> tree = new SortedTree<ClassAsTreeValue<string>>();
       AddNodeWholeTreeTestHelper(ref tree);
 
-      string expected = "CAS.Lib.RTLib.UTILS.Collections.Generic.SortedTree`1[CAS.Lib.RTLibCom.Tests.SortedTreeTest+ClassAsTreeValue`1[System.String]] elements:{(K(Height:5)[0:(A(Height:4)[0:(C(Height:1)[]1:(D(Height:3)[0:(F(Height:1)[]1:(G(Height:2)[0:(J(Height:1)[]]]]](B(Height:4)[0:(E(Height:3)[0:(H(Height:2)[0:(J(Height:1)[]]1:(I(Height:1)[]]]}";
+      string expected = "CAS.Lib.RTLib.Utils.Collections.Generic.SortedTree`1[CAS.Lib.RTLibCom.Tests.SortedTreeTest+ClassAsTreeValue`1[System.String]] elements:{(K(Height:5)[0:(A(Height:4)[0:(C(Height:1)[]1:(D(Height:3)[0:(F(Height:1)[]1:(G(Height:2)[0:(J(Height:1)[]]]]](B(Height:4)[0:(E(Height:3)[0:(H(Height:2)[0:(J(Height:1)[]]1:(I(Height:1)[]]]}";
       string actual = tree.ToString();
       Assert.AreEqual(expected, actual, "Wrong string representation");
     }
