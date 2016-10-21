33b241695a5e141cb4cc29340816869245fcb2fb
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2016-10-21 06:43:48 +0000

    CAS new release - working on CAS.RealTime  - merged two Unit Tests - preparing to merge the codebase too. Issue #: 4878

--- VS/trunk/CAS.RealTime/CAS.RealTimeEctension.UnitTests/Properties/AssemblyInfo.cs
+++ VS/trunk/CAS.RealTime/CAS.RealTimeEctension.UnitTests/Properties/AssemblyInfo.cs
@@ -1,25 +1,32 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿//<summary>
+//  Title   : Assembly info for: CAS.RealTime
+//  System  : Microsoft Visual C# .NET
+//  $LastChangedDate$
+//  $Rev$
+//  $LastChangedBy$
+//  $URL$
+//  $Id$
+//
+//  Copyright (c) 2000-2016 CAS LODZ POLAND
+//  +48 (42) 686 25 47
+//  techsupp@cas.eu
+//  www.cas.eu
+//</summary>
+
+using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("CAS.RTlib.TestProject")]
-[assembly: AssemblyDescription( "Tests for CAS.RTlib" )]
+[assembly: AssemblyTitle("CAS.RealTime.UnitTests")]
+[assembly: AssemblyDescription("CAS RealTime Library UnitTests")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CAS.RTlib.TestProject")]
-[assembly: AssemblyCopyright( "Copyright ©  2009 CAS" )]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCompany("CAS")]
+[assembly: AssemblyProduct("CAS Framework Core")]
+[assembly: AssemblyCopyright("Copyright (c) 2000-2016 CAS LODZ POLAND")]
+[assembly: AssemblyTrademark("CAS.RealTime-CAS Framework")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("f27654b8-7f7e-4ee1-bcd0-cf345d9bd9a1")]
-[assembly: AssemblyVersion( "1.0.0.*")]
-[assembly: AssemblyFileVersion("4.00.02")]
+[assembly: AssemblyVersion("5.0.4.*")]
+[assembly: AssemblyFileVersion("5.0.4")]
+
+
---
+++ VS/trunk/CAS.RealTime/CAS.RealTimeEctension.UnitTests/SettingsTest.cs
@@ -0,0 +1,22 @@
+
+using CAS.Lib.RTLib.Properties;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CAS.RealTime.UnitTests
+{
+  /// <summary>
+  /// Test Settings unit tests.
+  /// </summary>
+  [TestClass]
+  public class TestSettings
+  {
+    /// <summary>
+    /// SettingsTestFixture test
+    /// </summary>
+    [TestMethod]
+    public void SettingsTestFixture()
+    {
+      Assert.AreEqual("CAS.Lib.RTLib", Settings.Default.TraceName, "Trace name must be well defined and cannot be changed - it is breaking feature");
+    }
+  }
+}
