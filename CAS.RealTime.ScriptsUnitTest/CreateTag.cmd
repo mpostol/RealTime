7339ede9f2d978e1c1bb03670793180380fa3975
Mariusz <mpostol@cas.eu>
2016-10-23 20:52:06 +0000

    CAS new release - working on CAS.RealTime - created tag creation script 5.0.5 Issue #: 4878

---
+++ CAS.RealTime.ScriptsUnitTest/CreateTag.cmd
@@ -0,0 +1,13 @@
+
+rem//  $LastChangedDate:  $
+rem//  $Rev:  $
+rem//  $LastChangedBy:  $
+rem//  $URL:  $
+rem//  $Id:  $
+
+
+svn mkdir svn://svnserver.hq.cas.com.pl/VS/tags/CAS.RealTime.rel_5_0_5  -m "created new svn://svnserver.hq.cas.com.pl/VS/tags/CAS.RealTime.rel_5_0_5"
+svn copy svn://svnserver.hq.cas.com.pl/VS/trunk/CAS.RealTime/ svn://svnserver.hq.cas.com.pl/VS/tags/CAS.RealTime.rel_5_0_5 -m "created copy svn://svnserver.hq.cas.com.pl/VS/tags/CAS.RealTime.rel_5_0_5"
+
+pause ....
+
---
+++ CAS.RealTime.ScriptsUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,31 @@
+﻿//_______________________________________________________________
+//  Title   :  Assembly info for: CAS.RealTime.Scripts.UnitTest
+//  System  : Microsoft VisualStudio 2015 / C#
+//  $LastChangedDate:  $
+//  $Rev:  $
+//  $LastChangedBy: $
+//  $URL:  $
+//  $Id:  $
+//
+//  Copyright (c) 2000-2016 CAS LODZ POLAND
+//  +48 (42) 686 25 47
+//  techsupp@cas.eu
+//  www.cas.eu
+//_______________________________________________________________
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CAS.RealTime.Scripts.UnitTest")]
+[assembly: AssemblyDescription("Scripts Prototyping Workspace for CAS.RealTime.Scripts.UnitTest")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("CAS")]
+[assembly: AssemblyProduct("CAS CommServer Family")]
+[assembly: AssemblyCopyright("Copyright (c) 2000-2016 CAS LODZ POLAND")]
+[assembly: AssemblyTrademark("CommServer")]
+[assembly: AssemblyCulture("")]
+[assembly: Guid("1978B4D2-ACDF-4164-9677-BBEC3B858A45")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("5.0.5.*")]
+[assembly: AssemblyFileVersion("5.0.5")]
+
