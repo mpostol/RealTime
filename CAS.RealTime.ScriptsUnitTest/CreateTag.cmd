f6ee0b775c3aca0a2d1b4d595b67e5d01f90f08b
Mariusz <mpostol@cas.eu>
2016-11-03 12:02:44 +0000

    OPC VIewer Installing license has failed - working on the CAS.RealTime  - prepared for release: 5.0.6  - Unit Tested (one performance test failed.) Issue #: 4884

--- CAS.RealTime.ScriptsUnitTest/CreateTag.cmd
+++ CAS.RealTime.ScriptsUnitTest/CreateTag.cmd
@@ -6,8 +6,8 @@ rem//  $URL:  $
 rem//  $Id:  $
 
 
-svn mkdir svn://svnserver.hq.cas.com.pl/VS/tags/CAS.RealTime.rel_5_0_5  -m "created new svn://svnserver.hq.cas.com.pl/VS/tags/CAS.RealTime.rel_5_0_5"
-svn copy svn://svnserver.hq.cas.com.pl/VS/trunk/CAS.RealTime/ svn://svnserver.hq.cas.com.pl/VS/tags/CAS.RealTime.rel_5_0_5 -m "created copy svn://svnserver.hq.cas.com.pl/VS/tags/CAS.RealTime.rel_5_0_5"
+svn mkdir svn://svnserver.hq.cas.com.pl/VS/tags/CAS.RealTime.rel_5_0_6  -m "created new svn://svnserver.hq.cas.com.pl/VS/tags/CAS.RealTime.rel_5_0_6"
+svn copy svn://svnserver.hq.cas.com.pl/VS/trunk/CAS.RealTime/ svn://svnserver.hq.cas.com.pl/VS/tags/CAS.RealTime.rel_5_0_6 -m "created copy svn://svnserver.hq.cas.com.pl/VS/tags/CAS.RealTime.rel_5_0_6"
 
 pause ....
 
