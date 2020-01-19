200df083ad2a7e6400e1f3dacd9b4d54dcca6b1e
mpostol <mpostol@cas.eu>
2020-01-19 23:28:56 +0100

    Remove dependency on CodeProtect from the Real-time library #5
    - moved selected files from RealTime and created additional project.

--- CAS.RealTime.Core/Processes/EventLogMonitor.cs
+++ CAS.RealTime.Core/Processes/EventLogMonitor.cs
@@ -1,45 +1,34 @@
-//<summary>
-//  Title   : Events registration and events management
-//  System  : Microsoft Visual C# .NET 2008
-//  $LastChangedDate$
-//  $Rev$
-//  $LastChangedBy$
-//  $URL$
-//  $Id$
-//  History :
-//    MPostol 31-10-2003: created
-//    MPostol 26-04-2004:
-//      Wykonanie rejestracji EventLog.CreateEventSource wymaga podniesienia uprawnieñ, zatem mo¿e byæ 
-//      wykonane tylko w czasie normalnej instalacji. Na razie wywali³em, ale trzeba ten problem rozwi¹zaæ
-//    Wptasinski 11 - 05 -2004
-//      Exception handling added - whenever the log source cannot be created, exception is caught and handled
-//    20080612: mzbrzezny: Logs that goes to event log, goes to trace also.
-//                         This is first step to remove logging using eventLog.WriteEntry
-//                         and use only trace mechanism.
-//                         This change adds trace source to eventlogmonitor class, to use this 
-//                         trace add trace source: "TracesFromEventLogMonitor" to the application configuration
-//    20080625: mzbrzezny: new write to log function that supports category
+//___________________________________________________________________________________
 //
-//  Copyright (C)2006, CAS LODZ POLAND.
-//  TEL: +48 (42) 686 25 47
-//  mailto:techsupp@cas.eu
-//  http://www.cas.eu
-//</summary>
+//  Copyright (C) 2020, Mariusz Postol LODZ POLAND.
+//
+//  To be in touch join the community at GITTER: https://gitter.im/mpostol/OPC-UA-OOI
+//___________________________________________________________________________________
 
-using System;
 using System.Diagnostics;
-using System.Reflection;
 
 namespace CAS.Lib.RTLib.Processes
 {
+
+  ///<summary>
+  /// Specifies the event type of an event log entry.
+  ///</summary>
+  public enum EventLogEntryType
+  {
+    Error = 1, //An error event. This indicates a significant problem the user should know about; usually a loss of functionality or data.
+    FailureAudit = 16, //A failure audit event. This indicates a security event that occurs when an audited access attempt fails; for example, a failed attempt to open a file.
+    Information = 4, //An information event. This indicates a significant, successful operation.
+    SuccessAudit = 8, //A success audit event. This indicates a security event that occurs when an audited access attempt is successful; for example, logging on successfully.
+    Warning = 2 //A warning event. This indicates a problem that is not immediately significant, but that may signify conditions that could cause future problems.
+  }
+
   /// <summary>
   /// Summary description for EventLogMonitor.
   /// </summary>
   public class EventLogMonitor
   {
 
-    #region PRIVATE
-    #region trace
+    #region private
     private static TraceEventType ConvertToTraceEventType(EventLogEntryType eventLogEntryType)
     {
       switch (eventLogEntryType)
@@ -54,53 +43,23 @@ namespace CAS.Lib.RTLib.Processes
           return TraceEventType.Information;
       }
     }
-    #endregion
     private EventLogEntryType myType;
-    private int myEventID;
+    private readonly int myEventID;
     private short myCategory;
     private string myMessage;
-    #endregion
 
-    #region PRIVATE STATIC
+    #region static
     /// <summary>
     /// this name appears in the tracing messages
     /// </summary>
     private const string m_traceName = "TracesFromEventLogMonitor";
-    /// <summary>
-    /// elSource: The source by which the application is registered on the specified computer.
-    /// </summary>
-    private static string elSource = "CASProgDefName";
-    /// <summary>
-    /// elLogName: The name of the log the source's entries are written to. Possible values include: Application, 
-    /// Security, System, or a custom event log.
-    /// </summary>
-    private const string elLogName = "Application";
-    private static EventLog eventLog;
     private static TraceSource m_TraceSource;
     //static constructor
     static EventLogMonitor()
     {
-      Assembly _Assembly = Assembly.GetExecutingAssembly();
-      AssemblyProductAttribute progName = (AssemblyProductAttribute)Attribute.GetCustomAttribute(_Assembly, typeof(AssemblyProductAttribute), false);
-      if (progName != null)
-        elSource = progName.Product;
-      else
-        elSource = _Assembly.FullName;
-      try
-      {
-        if (!EventLog.SourceExists(elSource))
-          EventLog.CreateEventSource(elSource, elLogName);
-      }
-      catch
-      {
-        //bool x = false;
-      }
-      eventLog = new EventLog();
-      ((System.ComponentModel.ISupportInitialize)(eventLog)).BeginInit();
-      eventLog.Source = elSource;
-      ((System.ComponentModel.ISupportInitialize)(eventLog)).EndInit();
       m_TraceSource = new TraceSource(m_traceName);
-    }//EventLogMonitor
+    }
+    #endregion
     #endregion
 
     #region PUBLIC
@@ -124,7 +83,7 @@ namespace CAS.Lib.RTLib.Processes
     /// <param name="Message">The message.</param>
     /// <param name="level">The level.</param>
     /// <param name="eventID">The event ID.</param>
-    public static void WriteToEventLog(string Message, System.Diagnostics.EventLogEntryType level, int eventID)
+    public static void WriteToEventLog(string Message, EventLogEntryType level, int eventID)
     {
       WriteToEventLog(Message, level, eventID, 0);
     }
@@ -144,7 +103,7 @@ namespace CAS.Lib.RTLib.Processes
     /// </summary>
     /// <param name="Message">The message.</param>
     /// <param name="level">The level.</param>
-    public static void WriteToEventLog(string Message, System.Diagnostics.EventLogEntryType level)
+    public static void WriteToEventLog(string Message, EventLogEntryType level)
     {
       WriteToEventLog(Message, level, 0);
     }
@@ -171,30 +130,26 @@ namespace CAS.Lib.RTLib.Processes
     /// </summary>
     public void WriteEntry()
     {
-      eventLog.WriteEntry(myMessage, myType, myEventID, myCategory);
-      TraceEvent(ConvertToTraceEventType(myType), myEventID, System.String.Format("Category: {0}, {1}", myCategory, myMessage));
+      TraceEvent(ConvertToTraceEventType(myType), myEventID, string.Format("Category: {0}, {1}", myCategory, myMessage));
     }
     /// <summary>
     /// set the event message
     /// </summary>
     public string SetMessage
     {
-      set { myMessage = value; }
+      set => myMessage = value;
     }
     /// <summary>
     /// return previously set event message
     /// </summary>
-    public string GetMessage
-    {
-      get { return myMessage; }
-    }
+    public string GetMessage => myMessage;
     /// <summary>
     /// Sets the set category.
     /// </summary>
     /// <value>The set category.</value>
     public short SetCategory
     {
-      set { myCategory = value; }
+      set => myCategory = value;
     }
     /// <summary>
     ///  Creates an event log with the application-defined event identifier, and application-defined category 
@@ -213,5 +168,6 @@ namespace CAS.Lib.RTLib.Processes
       myCategory = category;
     }
     #endregion
+
   }
 }
