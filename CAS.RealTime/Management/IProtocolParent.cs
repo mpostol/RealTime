16a8924af7e48edfaa5aa695ccc47934df91f4f9
mpostol <mpostol@cas.eu>
2020-03-08 23:01:25 +0100

    Move files to one project #10
    - fixed problem with IProtocolParent.cs - it was in wrong place.
    - closing #10

--- CAS.RealTime/Management/IProtocolParent.cs
+++ CAS.RealTime/Management/IProtocolParent.cs
@@ -1,20 +1,9 @@
-﻿//<summary>
-//  Title   : IProtocolParent
-//  System  : Microsoft Visual C# .NET 2005
-//  $LastChangedDate$
-//  $Rev$
-//  $LastChangedBy$
-//  $URL$
-//  $Id$
-//  History :
-//    <Author> - <date>:
-//    <description>
+﻿//___________________________________________________________________________________
 //
-//  Copyright (C)2006, CAS LODZ POLAND.
-//  TEL: +48 (42) 686 25 47
-//  mailto:techsupp@cas.com.pl
-//  http:\\www.cas.eu
-//</summary>
+//  Copyright (C) 2020, Mariusz Postol LODZ POLAND.
+//
+//  To be in touch join the community at GITTER: https://gitter.im/mpostol/OPC-UA-OOI
+//___________________________________________________________________________________
 
 using System;
 
